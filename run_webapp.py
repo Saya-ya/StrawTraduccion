cf73e932e81fa435a6dbc6788000271a3e44397a
Allan Medina Martinez <allan.medina@jala.university>
2026-07-27 22:30:17 -0600

    Add fix in the local host

--- run_webapp.py
+++ run_webapp.py
@@ -3,5 +3,5 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("webapp.main:app", host="127.0.0.1", port=8080, reload=True,
+    uvicorn.run("webapp.main:app", host="0.0.0.0", port=8080, reload=True,
                 reload_dirs=["webapp"])
